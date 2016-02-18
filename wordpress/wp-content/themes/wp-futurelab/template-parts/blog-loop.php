4f29625bf8ff47a677f295d06c49a8a29a589647
Kevin <kevin@futurelab.co.nz>
2016-02-19 11:33:24 +1300

    added blog template

---
+++ wordpress/wp-content/themes/wp-futurelab/template-parts/blog-loop.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The template part for displaying single posts
+ *
+ * @package wp-futurelab
+ */
+?>
+<div class="col-md-4">
+  <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <header class="entry-header">
+      <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+    </header><!-- .entry-header -->
+
+    <?php wp_futurelab_post_thumbnail(); ?>
+
+    <div class="entry-content">
+      <?php
+        the_excerpt();
+
+      ?>
+    </div><!-- .entry-content -->
+  </article><!-- #post-## -->
+</div>
