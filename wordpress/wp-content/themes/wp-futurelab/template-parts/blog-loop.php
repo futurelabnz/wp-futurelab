8b96598715aabdb2dc45661137f44247cd22ba8d
Kevin <kevin@futurelab.co.nz>
2017-07-18 14:17:38 +1200

    blog template update

--- wordpress/wp-content/themes/wp-futurelab/template-parts/blog-loop.php
+++ wordpress/wp-content/themes/wp-futurelab/template-parts/blog-loop.php
@@ -7,17 +7,24 @@
 ?>
 <div class="col-md-4">
   <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+    <header class="entry-header">
+      <?php the_title( sprintf( '<h1 class="entry-title"><a href="%s">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+    </header><!-- .entry-header -->
 
-	<?php wp_futurelab_post_thumbnail(); ?>
+    <?php wp_futurelab_post_thumbnail(); ?>
 
-	<div class="entry-content">
-		<?php
-		the_excerpt();
-		do_action( 'wp_futurelab_blog-loop_after_excerpt' );
-		?>
-	</div><!-- .entry-content -->
+    <div class="entry-content">
+      <?php
+        $content_length = 150;
+        $shortDescription = get_the_excerpt();
+        if(strlen($shortDescription)>$content_length){
+          $shortDescription = substr($shortDescription,0,$content_length) . '...';
+        }
+        echo $shortDescription;
+      ?>
+      <div class="read-more-button">
+        <a href="<?php echo esc_url( get_permalink() ); ?>" class="read-more">Read More</a>
+      </div>
+    </div><!-- .entry-content -->
   </article><!-- #post-## -->
 </div>
